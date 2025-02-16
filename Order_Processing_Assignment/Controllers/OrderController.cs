39ca7f86ea6085af058a42635f08a94f22377c4d
Mukul pal <you@example.com>
2025-02-16 17:05:07 +0530

    Initial commit - Order Processing Application

---
+++ Order_Processing_Assignment/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using Order_Processing_Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Order_Processing_Assignment.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessOrder(string customerName, string email, decimal orderAmount, string customerType)
+        {
+            decimal discount = CalculateDiscount(orderAmount, customerType);
+            decimal finalTotal = orderAmount - discount;
+
+            ViewBag.CustomerName = customerName;
+            ViewBag.Email = email;
+            ViewBag.OrderAmount = orderAmount;
+            ViewBag.Discount = discount;
+            ViewBag.FinalTotal = finalTotal;
+            ViewBag.CustomerType = customerType;
+
+            return View("Result");
+        }
+
+        private decimal CalculateDiscount(decimal amount, string customerType)
+        {
+            if (amount >= 100 && customerType == "Loyal")
+            {
+                return amount * 0.10m; 
+            }
+            return 0;
+        }
+    }
+}
---
+++ Order_Processing_Assignment/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Order_Processing_Assignment.Models
+{
+    public class Order
+    {
+        public decimal Amount { get; set; }
+        public string CustomerType { get; set; }
+        public decimal Discount { get; set; }
+        public decimal FinalTotal { get; set; }
+    }
+
+}
