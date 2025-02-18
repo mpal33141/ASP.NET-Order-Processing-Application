81e9e9b208081572359f56553a9054e35e652bde
Mukul pal <you@example.com>
2025-02-18 10:20:47 +0530

    Added Unit Test Project

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("777d8dc4-be7a-4f3f-b551-ac6c09d4b6d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Order_Processing_Assignment.Controllers;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Discount_ShouldBe10Percent_WhenLoyalCustomerAndOrderAbove100()
+        {
+            // Arrange
+            var controller = new OrderController();
+            decimal orderAmount = 150;  
+            string customerType = "Loyal";
+
+            // Act
+            var discount = controller.GetType()
+                                     .GetMethod("CalculateDiscount", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                                     .Invoke(controller, new object[] { orderAmount, customerType });
+
+            // Assert
+            Assert.AreEqual(15, (decimal)discount); 
+        }
+    }
+}
